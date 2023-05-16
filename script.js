fc47ea4b843c37c799cc590d5027865902f9cba8
DevD1k <113674752+DevD1k@users.noreply.github.com>
2023-05-16 17:17:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-// complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+	this.greet = ()=>{
+		console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	// Person.call(this, name, age);
+	this.name = name;
+	this.age = age;
+	this.jobTitle = jobTitle;
+	this.jobGreet = ()=>{
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
+	}
+}
+// Employee.prototype = Object.create(Person.prototype);
+
 
 // Do not change code below this line
 window.Person = Person;
